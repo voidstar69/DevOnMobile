c1f36d4054ac58833b0580a464c4c5da71447fc8
Gavin Murrison <github@ninemail.net>
2017-05-08 18:13:34 +0100

    Update PrimeTests.cs

--- DevOnMobileTests/PrimeTests.cs
+++ DevOnMobileTests/PrimeTests.cs
@@ -15,7 +15,7 @@ namespace DevOnMobile.Tests
   {
    Console.WriteLine("Testing numbers for Primality");
    var prime = new Prime();
-   for (int i = 1000000; i > 0; i--)
+   for (int i = 100; i > 0; i--)
    {
     bool isPrime1 = prime.IsPrimeSlow(i);
     bool isPrime2 = prime.IsPrimeFast(i);
