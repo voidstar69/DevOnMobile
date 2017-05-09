0be1b77bbb2a2149cc9be2d6288f60847ab9aa2a
Gavin Murrison <github@ninemail.net>
2017-05-09 13:06:56 +0100

    Update A.cs

--- A.cs
+++ A.cs
@@ -14,12 +14,12 @@ namespace DevOnMobile
 
   public string encode(string data)
   {
-   return data;
+   return data.Reverse();
   }
 
   public string decode(string data)
   {
-   return data;
+   return data.Reverse();
   }
 
 /*
