26772c8330a0e34ae425892fd92fd2a7bb2b7956
Gavin Murrison <github@ninemail.net>
2017-05-20 15:31:42 +0100

    Update B.cs

--- B.cs
+++ B.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-//using System.Collections;using System.Collections.Generic;
+using System.Collections;//using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevOnMobile
 {
- public class BinaryTree : IEnumerable<int>
+ public class BinaryTree : IEnumerable
  {
   public void Add(int item)
   {
   }
 
+/*
   public IEnumerator<int> GetEnumerator()
   {
    return new Enumerator<int>();
   }
+*/
 
   private IEnumerator GetEnumerator()
   {
