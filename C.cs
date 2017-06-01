e1a16afe101407b2cbba73c14120076f8721f330
Gavin Murrison <github@ninemail.net>
2017-06-01 14:04:15 +0100

    Update C.cs

--- C.cs
+++ C.cs
@@ -8,7 +8,7 @@ namespace DevOnMobile
 {
  public class Interpreter
  {
-  public void Execute(string program, Stream output)
+  public void Execute(string program, TextWriter output)
   {
    var lines = program.Split(program, '\n');
    foreach(var line in lines)
