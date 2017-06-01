4151ff6fa3685a0120ce9c4248bc9fb7bab3e7a3
Gavin Murrison <github@ninemail.net>
2017-06-01 14:12:44 +0100

    Update C.cs

--- C.cs
+++ C.cs
@@ -14,7 +14,18 @@ namespace DevOnMobile
    var lines = program.Split('\n');
    foreach(var line in lines)
    {
+    output.Write(">> ");
     output.WriteLine(line);
+
+    var tokens = line.Split(' ', 2);
+    var cmd = tokens[0];
+    var data = tokens[1];
+    switch(data)
+    {
+     "print":
+      output.Write(data);
+      break;
+    }
    }
   }
  }
