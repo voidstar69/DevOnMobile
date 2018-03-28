fc4009fd63cea3d04dfe20dd1f340e061b10be0d
voidstar69 <voidstar69@github.com>
2018-03-28 23:03:30 +0100

    Converted Prime MS-tests to xUnit tests

--- DevOnMobileTests/BTests.cs
+++ DevOnMobileTests/BTests.cs
@@ -6,26 +6,25 @@ namespace DevOnMobile.Tests
  public class BinaryTreeTests
  {
   [Fact]
-  public void testBinaryTree()
+  public void TestBinaryTree()
   {
-   var tree = new BinaryTree<int>();
-   tree.Add(5);
-   tree.Add(8);
-   tree.Add(2);
+   var tree = new BinaryTree<int> {5, 8, 2};
 
    Console.WriteLine("Binary tree contents in order:");
-   foreach (var item in tree)
+   foreach (int item in tree)
    {
     Console.WriteLine(item);
    }
 
    // TODO: tree should put numbers into order
    int[] expectedItemOrder = { 2, 8, 5 };
-   var enu = tree.GetEnumerator();
-   foreach (var expItem in expectedItemOrder)
+   using(var enu = tree.GetEnumerator())
    {
-    Assert.True(enu.MoveNext());
-    Assert.Equal(expItem, enu.Current);
+    foreach (int expItem in expectedItemOrder)
+    {
+     Assert.True(enu.MoveNext());
+     Assert.Equal(expItem, enu.Current);
+    }
    }
   }
  }
