6284bb6bdc1b40b20adfb7c62084e86937e63182
voidstar69 <voidstar69@github.com>
2018-03-29 21:40:55 +0100

    Made prime tests loop less to avoid slowing down code coverage too much

--- DevOnMobileTests/PrimeTests.cs
+++ DevOnMobileTests/PrimeTests.cs
@@ -5,12 +5,12 @@ namespace DevOnMobile.Tests
 {
  public class PrimeTests
  {
-//  [Fact]
+  [Fact]
   public void TestIsPrime()
   {
-   Console.WriteLine("Testing numbers for Primality");
+   Console.WriteLine("TestIsPrime: starting");
    var prime = new Prime();
-   for (var i = 1000000; i > 0; i--)
+   for (var i = 10000; i > 0; i--)
    {
     bool isPrime1 = Prime.IsPrimeSlow(i);
     bool isPrime2 = prime.IsPrimeFast(i);
@@ -19,26 +19,31 @@ namespace DevOnMobile.Tests
     Assert.Equal(isPrime1, isPrime2);
 //    Assert.AreEqual(isPrime1, isPrime2, string.Format("Num: {0}, Slow: {1}, Fast: {2}\nDebug slow: {3}\nDebug fast: {4}", i, isPrime1, isPrime2, prime.DebugSlow, prime.DebugFast));
    }
+   Console.WriteLine("TestIsPrime: finished");
   }
 
-//  [Fact]
+  [Fact]
   public void IsPrimeFastPerformance()
   {
+   Console.WriteLine("IsPrimeFastPerformance: starting");
    var prime = new Prime();
-   for (var i = 1; i < 10000000; i++)
+   for (var i = 1; i < 10000; i++)
    {
     prime.IsPrimeFast(i);
    }
+   Console.WriteLine("IsPrimeFastPerformance: finished");
   }
 
-//  [Fact]
+  [Fact]
   public void IsPrimeSlowPerformance()
   {
+   Console.WriteLine("IsPrimeSlowPerformance: starting");
    var prime = new Prime();
-   for (var i = 1; i < 10000000; i++)
+   for (var i = 1; i < 10000; i++)
    {
     Prime.IsPrimeSlow(i);
    }
+   Console.WriteLine("IsPrimeSlowPerformance: finished");
   }
  }
 }
