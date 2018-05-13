cc982f5041f63a33dd7248a9a4c83a42b2a88ebf
voidstar69 <voidstar69@github.com>
2018-05-13 16:50:34 +0100

    Created .NET Standard library with copy of Codec code, and test projects for .NET Core and .NET Framework.

---
+++ CodeTestsFramework/CodecTests.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Codec;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodecTestsFramework
+{
+    [TestClass]
+ public class CodecTests
+ {
+  [TestMethod, Timeout(100)]
+  public void testGZip()
+  {
+   var input = genRandomBytes(1000);
+
+   using (var outMemStream = new MemoryStream())
+   {
+    using (var inMemStream = new MemoryStream(input))
+    using (var zipStream = new GZipStream(outMemStream, CompressionMode.Compress))
+    {
+     inMemStream.CopyTo(zipStream);
+    }
+
+    var output = outMemStream.GetBuffer();
+    Console.WriteLine("GZip: {0}% ({1} bytes)", (double)output.Length / input.Length * 100, output.Length);
+   }
+  }
+
+  [TestMethod, Timeout(100)]
+  public void testDeflate()
+  {
+   var input = genRandomBytes(1000);
+
+   using (var outMemStream = new MemoryStream())
+   {
+    using (var inMemStream = new MemoryStream(input))
+    using (var zipStream = new DeflateStream(outMemStream, CompressionMode.Compress))
+    {
+
+     // TODO
+//     checkStreamEncode(zipStream, outMemStream, input);
+
+     inMemStream.CopyTo(zipStream);
+    }
+
+    var output = outMemStream.GetBuffer();
+    Console.WriteLine("Deflate: {0}% ({1} bytes)", (double)output.Length / input.Length * 100, output.Length);
+   }
+  }
+
+  private byte[] genRandomBytes(int len)
+  {
+   var data = new byte[len];
+   var random = new Random();
+   random.NextBytes(data);
+   return data;
+
+/*
+   byte currByte = 0;
+
+   for(int j=0; j<len; j++)
+   {
+    if(random.NextDouble() < byteChangeProb)
+     currByte=random.NextByte();
+
+    data[j] = currByte;
+   }
+
+   return data;
+*/
+  }
+
+  private byte[] genRandomBytes(int len, double byteChangeProb)
+  {
+   var data = new byte[len];
+   var random = new Random();
+   byte currByte = 0;
+
+   for(int j=0; j<len; j++)
+   {
+    if(random.NextDouble() < byteChangeProb)
+     currByte = (byte)random.Next(256);
+
+    data[j] = currByte;
+   }
+
+   return data;
+  }
+
+  private string genText(int len, double charChangeProb)
+  {
+   var text = string.Empty;
+   var random = new Random();
+   char ch = ' ';
+
+   for(int j=0; j<len; j++)
+   {
+    if(random.NextDouble() < charChangeProb)
+     ch=(char)('a'+random.Next(26));
+
+    text += ch;
+   }
+
+   return text;
+  }
+
+  private byte[] checkStreamEncode(Stream encodeStream, MemoryStream outStream, byte[] input)
+  {
+   using (var inMemStream = new MemoryStream(input))
+   using (encodeStream)
+   {
+    inMemStream.CopyTo(encodeStream);
+   }
+
+   byte[] output = outStream.GetBuffer();
+   Console.WriteLine("Stream Encode: {0} bytes -> {1} bytes = {2}% compression",
+    input.Length, output.Length, (double)output.Length / input.Length * 100);
+   return output;
+  }
+
+  private string checkCodec(Codec.Codec codec, string input, string expectedEncoded)
+  {
+   var encoded = codec.encode(input);
+   var output = codec.decode(encoded);
+   Console.WriteLine("{0} -> {1}", input, encoded);
+
+   if (expectedEncoded != null)
+    Assert.AreEqual(expectedEncoded, encoded); //, "Unexpected encoded data");
+
+   Assert.AreEqual(input, output); //, "Encode then decode must produce original data");
+
+   // TODO: this fails for the binary RLE codec
+   //Assert.True(encoded.Length <= input.Length, "Codec must not expand data");
+
+   //Assert.AreNotEqual(input, encoded, "Encoding must change data");
+
+   return encoded;
+  }
+
+  [TestMethod, Timeout(150)]
+  public void testCharacterRunLengthCodecWithRandomData()
+  {
+   var random = new Random();
+   var codec1 = new CharacterRunLengthCodec();
+   var totalDecodedSize = 0;
+   var totalEncodedSize = 0;
+
+   for(int i=0; i<50; i++)
+   {
+    var input = "";
+    char ch = ' ';
+
+    for(int j=0; j<20; j++)
+    {
+     if(random.NextDouble()<0.5)
+      ch=(char)('a'+random.Next(26));
+
+     input += ch;
+    }
+
+    var encoded = checkCodec(codec1, input, null);
+    totalDecodedSize += input.Length;
+    totalEncodedSize += encoded.Length;
+   }
+
+   Console.WriteLine();
+   Console.WriteLine("*** Compression ratio: {0}% (encoded size vs original size) ***", (double)totalEncodedSize / totalDecodedSize * 100);
+  }
+
+  [TestMethod, Timeout(100)]
+  public void testBinaryRunLengthCodecWithRandomData()
+  {
+   var random = new Random();
+   var codec1 = new BinaryRunLengthCodec();
+   var totalDecodedSize = 0;
+   var totalEncodedSize = 0;
+
+   for (int i = 0; i < 50; i++)
+   {
+    var input = "";
+    char ch = '0';
+
+    for (int j = 0; j < 20; j++)
+    {
+     if (random.NextDouble() < 0.5)
+      ch = (random.Next(2) == 0 ? '0' : '1');
+
+     input += ch;
+    }
+
+    var encoded = checkCodec(codec1, input, null);
+    totalDecodedSize += input.Length;
+    totalEncodedSize += encoded.Length;
+   }
+
+   Console.WriteLine();
+   Console.WriteLine("*** Compression ratio: {0}% (encoded size vs original size) ***", (double)totalEncodedSize / totalDecodedSize * 100);
+  }
+
+  [TestMethod, Timeout(1000)]
+  public void testHuffmanCodecWithRandomData()
+  {
+   var random = new Random();
+   var codec1 = new HuffmanCodec();
+   var totalDecodedSize = 0;
+   var totalEncodedSize = 0;
+
+   for (int i = 0; i < 20; i++)
+   {
+    var input = "";
+    char ch = ' ';
+
+    for (int j = 0; j < 20; j++)
+    {
+     if (random.NextDouble() < 0.5)
+      ch = (char)('a' + random.Next(26));
+
+     input += ch;
+    }
+
+    var encoded = checkCodec(codec1, input, null);
+    totalDecodedSize += input.Length * 8;
+    totalEncodedSize += encoded.Length;
+   }
+
+   Console.WriteLine();
+   Console.WriteLine("*** Compression ratio: {0}% (encoded size vs original size, in bits) ***", (double)totalEncodedSize / totalDecodedSize * 100);
+  }
+
+  [TestMethod, Timeout(400)]
+  public void testMultipleCodecs()
+  {
+   var input3 = "a";
+   var output3 = "a";
+   var input4 = "aa";
+   var output4 = "a2";
+   var input5 = "aab";
+   var output5 = "a2b";
+   var input6 = "";
+   var output6 = "";
+
+   var codec1 = new CharacterRunLengthCodec();
+   var codec2 = new BinaryRunLengthCodec();
+   var codec3 = new HuffmanCodec();
+
+   checkCodec(codec1, "Hello Wooorld", "Hel2o Wo3rld");
+   checkCodec(codec1, "hhheelooo   woorrrlllld!!", "h3e2lo3 3wo2r3l4d!2");
+   checkCodec(codec1, input3, output3);
+   checkCodec(codec1, input4, output4);
+   checkCodec(codec1, input5, output5);
+   checkCodec(codec1, input6, output6);
+
+   checkCodec(codec2, "0", "0");
+   checkCodec(codec2, "1", "1");
+   checkCodec(codec2, "00", "0000");
+   checkCodec(codec2, "11", "1100");
+   checkCodec(codec2, "", "");
+   checkCodec(codec2, "10001", "100011");
+
+   checkCodec(codec3, "Hello Wooorld", "0001101001101010011101001101100111110110001000001001000100100111101010100100110110100001011011001110101010001011111");
+   checkCodec(codec3, "hhheelooo   woorrrlllld!!", "001111101100010010011011110111010000010000100010110101001110001100001001101001101001101101001001001101110111100000001101101101010000101101101111111111111010011001100");
+
+   var input7 = genText(1000, 1.0);
+   Console.WriteLine("Compression ratio on {0} random letters (encoded size vs original size):", input7.Length);
+
+   var encoded = codec1.encode(input7);
+   Console.WriteLine("Char RLE: {0}%", (double)encoded.Length / input7.Length * 100);
+
+   encoded = codec3.encode(input7);
+   Console.WriteLine("Huffman: {0}%", (double)encoded.Length / 8.0 / input7.Length * 100);
+  }
+
+/*
+  [TestMethod]
+  public void encodeThenDecodeEmptyDataMustProduceOriginalData()
+  {
+   const string input = "";
+   var codec = new CharacterRunLengthCodec();
+   var encoded = codec.encode(input);
+   var output = codec.decode(encoded);
+   Assert.Equal(input, output);
+  }
+
+  [TestMethod]
+  public void encodeThenDecodeManyRepeatedCharsMustProduceOriginalData()
+  {
+   const string input = "qdttpmmmmmmmmmmhmm";
+   var codec = new CharacterRunLengthCodec();
+   var encoded = codec.encode(input);
+   var output = codec.decode(encoded);
+   Assert.Equal(input, output);
+  }
+
+  [TestMethod]
+  public void encodeThenDecodeMustProduceOriginalData()
+  {
+   //const string input = "Hello World";
+   var input = "hhheelooo   woorrrlllld!!";
+   var expectedEncoded = "h3e2lo3 3wo2r3l4d!2";
+
+   var codec = new CharacterRunLengthCodec();
+   var encoded = codec.encode(input);
+   Assert.Equal(expectedEncoded, encoded, "Unexpected encoded data");
+   var output = codec.decode(encoded);
+   Assert.Equal(input, output);
+  }
+
+  [TestMethod]
+  public void verifyRunLengthEncodedData()
+  {
+   const string input = "Hello Wooorld";
+   var codec = new CharacterRunLengthCodec();
+   var encoded = codec.encode(input);
+   Assert.Equal("Hel2o Wo3rld", encoded);
+  }
+*/
+
+  [TestMethod]
+  public void codecMustNotExpandData()
+  {
+   const string input = "Hello World";
+   var codec = new CharacterRunLengthCodec();
+   var encoded = codec.encode(input);
+   var output = codec.decode(encoded);
+   Assert.IsTrue(encoded.Length <= input.Length);
+  }
+ }
+}
---
+++ Codec/ICodec.cs
@@ -0,0 +1,464 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Codec
+{
+    public interface ICodec
+    {
+        string encode(string data);
+        string decode(string data);
+    }
+
+    public class BitStream
+    {
+        private string data;
+
+        public BitStream()
+        {
+            data = string.Empty;
+        }
+
+        public BitStream(string data)
+        {
+            this.data = data;
+        }
+
+        public int? ReadBit()
+        {
+            if (data.Length == 0)
+                return null;
+
+            char ch = data[0];
+            data = data.Substring(1);
+            var bit = int.Parse(ch.ToString());
+
+            if (bit != 0 && bit != 1)
+                throw new ArgumentOutOfRangeException("bit", bit, "bit must be 0 or 1");
+            return bit;
+        }
+
+        public void WriteBit(int bit)
+        {
+            if (bit != 0 && bit != 1)
+                throw new ArgumentOutOfRangeException("bit", bit, "bit must be 0 or 1");
+            data += bit.ToString();
+        }
+
+        public byte ReadByte()
+        {
+            byte value = 0;
+            for (int bitPos = 1; bitPos <= 8; bitPos++)
+            {
+                value >>= 1;
+                value |= (byte) (ReadBit() * 0x80);
+            }
+
+            return value;
+        }
+
+        public void WriteByte(byte value)
+        {
+            for (int bitPos = 1; bitPos <= 8; bitPos++)
+            {
+                WriteBit(value & 1);
+                value >>= 1;
+            }
+        }
+
+        public string GetData()
+        {
+            return data;
+        }
+
+        public int Length
+        {
+            get { return data.Length; }
+        }
+    }
+
+    public class HuffmanCodec : ICodec
+    {
+        public static TextWriter log;
+
+        static HuffmanCodec()
+        {
+#if DEBUG
+            //log = Console.Out;
+            log = TextWriter.Null;
+#else
+   log = TextWriter.Null;
+#endif
+        }
+
+        // TODO: use [Serializable] ?
+        private class Node // TODO: this represents leaf nodes. Adapt it to also represent internal nodes.
+        {
+            public byte byteValue;
+            public int frequency; // number of occurences of this byte value. TODO: we may be able to get rid of this
+
+            public Node parent;
+
+            // TODO: leaf nodes do not need children pointers
+            public Node leftChild;
+            public Node rightChild;
+
+            public bool IsLeaf()
+            {
+                return leftChild == null && rightChild == null;
+            }
+
+            // Serialise the tree (rooted at this node) into the bit stream.
+            // This only serialises data required by the decoder.
+            public void Serialise(BitStream stream)
+            {
+                // TODO: serialise byteValue, leftChild and rightChild
+                if (IsLeaf())
+                {
+                    log.WriteLine("Leaf node {0} (0x{0:X})", byteValue);
+                    stream.WriteBit(1);
+                    stream.WriteByte(byteValue);
+                }
+                else
+                {
+                    log.WriteLine("Internal node");
+                    stream.WriteBit(0);
+                    leftChild.Serialise(stream);
+                    rightChild.Serialise(stream);
+                }
+            }
+
+            // Deserialise the tree (rooted at this node) from the bit stream.
+            // This only deserialises data required by the decoder.
+            public void Deserialise(BitStream stream)
+            {
+                bool isLeaf = (stream.ReadBit() == 1);
+                if (isLeaf)
+                {
+                    byteValue = stream.ReadByte();
+                    log.WriteLine("Leaf node {0} (0x{0:X})", byteValue);
+                }
+                else
+                {
+                    log.WriteLine("Internal node");
+                    leftChild = new Node();
+                    leftChild.Deserialise(stream);
+                    rightChild = new Node();
+                    rightChild.Deserialise(stream);
+                }
+            }
+        }
+
+        public string encode(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var encoding = new ASCIIEncoding();
+            byte[] rawBytes = encoding.GetBytes(text);
+            var freqtoTreeDict =
+                new SortedList<int, IList<Node>>(rawBytes.Length); // TODO: guess number of internal 'tree' nodes?
+            var byteToNodeDict = new Dictionary<byte, Node>(byte.MaxValue);
+            int[] frequency = new int[byte.MaxValue + 1];
+
+            using (MemoryStream inputStream = new MemoryStream(rawBytes))
+            {
+                // track frequency (number of occurences) of each unique byte value
+                int byteOrFlag;
+                while (-1 != (byteOrFlag = inputStream.ReadByte()))
+                {
+                    byte num = (byte) byteOrFlag;
+                    frequency[num]++;
+                }
+
+                // build a collection of (tree) nodes, one per unique byte value that occurs in the input
+                for (int num = byte.MinValue; num <= byte.MaxValue; num++)
+                {
+                    var byteValue = (byte) num;
+                    var freq = frequency[num];
+                    if (freq > 0)
+                    {
+                        var node = new Node {byteValue = byteValue, frequency = freq};
+                        byteToNodeDict.Add(byteValue, node);
+
+                        if (!freqtoTreeDict.ContainsKey(freq))
+                            freqtoTreeDict[freq] = new List<Node>();
+                        freqtoTreeDict[freq].Add(node);
+                    }
+                }
+
+                // loop until there is only one root node, i.e. a single tree
+                while (freqtoTreeDict.Count > 1 || freqtoTreeDict.First().Value.Count > 1)
+                {
+                    // find the two nodes/trees with lowest frequency
+                    var nodeList = freqtoTreeDict.First().Value;
+                    var lowFreqNode1 = nodeList.First();
+                    nodeList.RemoveAt(0);
+                    if (nodeList.Count == 0)
+                        freqtoTreeDict.RemoveAt(0);
+
+                    nodeList = freqtoTreeDict.First().Value;
+                    var lowFreqNode2 = nodeList.First();
+                    nodeList.RemoveAt(0);
+                    if (nodeList.Count == 0)
+                        freqtoTreeDict.RemoveAt(0);
+
+                    // combine these two nodes/trees
+                    // TODO: internal node should not have byteValue!
+                    var parent = new Node
+                    {
+                        leftChild = lowFreqNode1,
+                        rightChild = lowFreqNode2,
+                        frequency = lowFreqNode1.frequency + lowFreqNode2.frequency
+                    };
+                    lowFreqNode1.parent = parent;
+                    lowFreqNode2.parent = parent;
+
+                    if (!freqtoTreeDict.ContainsKey(parent.frequency))
+                        freqtoTreeDict[parent.frequency] = new List<Node>();
+                    freqtoTreeDict[parent.frequency].Add(parent);
+                }
+
+                // Huffman coding tree has now been built. Get tree root node.
+                Node treeRoot = freqtoTreeDict.First().Value.Single();
+
+                // seek to the start of the input data
+                long newPos = inputStream.Seek(0, SeekOrigin.Begin);
+                if (newPos != 0)
+                    throw new ArgumentOutOfRangeException("Seek position", newPos,
+                        "Seeked a MemoryStream to the start but it returned a different position!");
+
+                // store the Huffman coding tree in the bitstream, so the decoder can reconstruct the tree
+                log.WriteLine("Serialising Huffman tree:");
+                var outputStream = new BitStream();
+                treeRoot.Serialise(outputStream);
+
+                // translate each input byte value into a variable number of bits
+                log.WriteLine("Huffman encoding each byte to a variable number of bits:");
+                while (-1 != (byteOrFlag = inputStream.ReadByte()))
+                {
+                    byte num = (byte) byteOrFlag;
+
+                    // use a stack to write the bits in reverse order
+                    var stack = new Stack<int>(8); // TODO: reverse bits more efficiently?
+
+                    log.Write("{0}=", num);
+                    for (var node = byteToNodeDict[num]; node.parent != null; node = node.parent)
+                    {
+                        int bit = (node == node.parent.leftChild ? 0 : 1);
+                        stack.Push(bit);
+                        log.Write(bit);
+                    }
+
+                    log.Write('/');
+
+                    foreach (int bit in stack)
+                    {
+                        outputStream.WriteBit(bit);
+                        log.Write(bit);
+                    }
+
+                    log.WriteLine();
+                }
+
+                return outputStream.GetData();
+            }
+        }
+
+        public string decode(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            // reconstruct the Huffman coding tree from the bitstream
+            var input = new BitStream(text);
+            var treeRoot = new Node();
+            log.WriteLine("Deserialising Huffman tree:");
+            treeRoot.Deserialise(input);
+
+            log.WriteLine("Huffman decoding groups of bits into bytes:");
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                var node = treeRoot;
+                int? bitOrNull;
+                while (null != (bitOrNull = input.ReadBit()))
+                {
+                    int bit = bitOrNull.Value;
+                    node = (bit == 0 ? node.leftChild : node.rightChild);
+                    log.Write(bit);
+
+                    if (node.IsLeaf())
+                    {
+                        outputStream.WriteByte(node.byteValue);
+                        log.WriteLine("={0}", node.byteValue);
+                        node = treeRoot; // reset to root of tree
+                    }
+                }
+
+                return new ASCIIEncoding().GetString(outputStream.ToArray());
+            }
+        }
+    }
+
+    public class BinaryRunLengthCodec : ICodec
+    {
+        public string encode(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var input = new BitStream(text);
+            var output = new BitStream();
+
+            int runLen = 1;
+            int prevBit = input.ReadBit().Value;
+            bool outOfBits = false;
+
+            while (!outOfBits)
+            {
+                int? bitOrNull = input.ReadBit();
+                int bit = bitOrNull.HasValue ? bitOrNull.Value : -1;
+                outOfBits = !bitOrNull.HasValue;
+
+                if (bit == prevBit && runLen < 5)
+                {
+                    runLen++;
+                }
+                else
+                {
+                    output.WriteBit(prevBit);
+                    if (runLen > 1)
+                    {
+                        output.WriteBit(prevBit);
+                        runLen -= 2;
+                        output.WriteBit(runLen >> 1);
+                        output.WriteBit(runLen & 1);
+                    }
+
+                    runLen = 1;
+                }
+
+                prevBit = bit;
+            }
+
+            return output.GetData();
+        }
+
+        public string decode(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var input = new BitStream(text);
+            var output = new BitStream();
+
+            int bit = input.ReadBit().Value;
+            bool outOfBits = false;
+
+            while (!outOfBits)
+            {
+                int prevBit = bit;
+
+                int? bitOrNull = input.ReadBit();
+                bit = bitOrNull ?? -1;
+                outOfBits = !bitOrNull.HasValue;
+
+                if (bit != prevBit)
+                {
+                    output.WriteBit(prevBit);
+                }
+                else
+                {
+                    int runLen = (input.ReadBit().Value << 1) + input.ReadBit().Value + 2;
+
+                    for (int j = 0; j < runLen; j++)
+                    {
+                        output.WriteBit(bit);
+                    }
+
+                    bit = input.ReadBit() ?? -1;
+                    if (bit == -1)
+                        outOfBits = true;
+                }
+            }
+
+            return output.GetData();
+        }
+    }
+
+    // run length encoding where run length is stored in next character
+    public class CharacterRunLengthCodec : ICodec
+    {
+        public string encode(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+                return data;
+
+            string output = "";
+            int runLen = 1;
+            char prevCh = data[0];
+
+            for (int i = 1; i <= data.Length; i++)
+            {
+                char ch = (i == data.Length ? '\0' : data[i]);
+
+                if (ch == prevCh && runLen < 9)
+                {
+                    runLen++;
+                }
+                else
+                {
+                    output += prevCh;
+                    if (runLen > 1)
+                    {
+                        output += runLen;
+                    }
+
+                    runLen = 1;
+                }
+
+                prevCh = ch;
+            }
+
+            return output;
+        }
+
+        public string decode(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+                return data;
+
+            string output = "";
+            char ch = data[0];
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                char prevCh = ch;
+                ch = data[i];
+
+                if (!char.IsDigit(ch))
+                {
+                    output += prevCh;
+                }
+                else
+                {
+                    int runLen = ch - '0';
+                    for (int j = 0; j < runLen; j++)
+                    {
+                        output += prevCh;
+                    }
+
+                    i++;
+                    ch = (i == data.Length ? '\0' : data[i]);
+                }
+            }
+
+            if (ch != '\0')
+                output += ch;
+
+            return output;
+        }
+    }
+}
