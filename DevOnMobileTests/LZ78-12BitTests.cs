fccb6f8bd354d7ad43e1473a9226fe22fae7eb49
voidstar69 <voidstar69@github.com>
2019-04-21 20:52:54 +0100

    Avoid timeouts in new large data tests

--- DevOnMobileTests/LZ78-12BitTests.cs
+++ DevOnMobileTests/LZ78-12BitTests.cs
@@ -31,14 +31,14 @@ namespace DevOnMobile.Tests
             CodecTestUtils.CheckStreamCodecWithBinaryData(new LempelZiv78_12BitCodec(), input, new byte[]{0,0,1,0,0,2,1,0,2,0,0,3,3,0,8});
         }
 
-        [TestMethod, Timeout(1000)]
+        [TestMethod, Timeout(10000)]
         public void TestWithLargeData()
         {
             byte[] encodedBytes = CodecTestUtils.CheckStreamCodecWithBinaryData(new LempelZiv78_12BitCodec(), RandomBytes, null, false);
             Console.WriteLine("LZ78-12bit: {0}% ({1}->{2} bytes)", (double) encodedBytes.Length / RandomBytes.Length * 100, RandomBytes.Length, encodedBytes.Length);
         }
 
-        [TestMethod, Timeout(1000)]
+        [TestMethod, Timeout(10000)]
         public void TestFor64KBoundaryBug()
         {
             byte[] veryRandomBytes = CodecTestUtils.GenRandomBytes(256 * 1024, 1.0);
